4626e53c0a3813f1c0cb71bb385e17a8d765b678
Dot8Pixels <napatsriz@gmail.com>
2024-02-04 22:26:42 +0700

    feat: :sparkles: Control Flow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let score: i32 = 65;
+
+    if score > 80 {
+        println!("A");
+    } else if score >= 70 {
+        println!("B")
+    } else if score >= 60 {
+        println!("C")
+    } else if score >= 50 {
+        println!("D")
+    } else {
+        println!("E")
+    }
 }
