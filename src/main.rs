9fe5b2b1da54a72b7809ae7d37051884efb269c4
Dot8Pixels <napatsriz@gmail.com>
2024-02-04 22:20:31 +0700

    feat: :sparkles: Function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let x: i32 = 10;
+    let y: i32 = 5;
+    let result: i32 = add(x, y);
+
+    println!("{} + {} = {}", x, y, result);
+}
+
+fn add(x: i32, y: i32) -> i32 {
+    x + y
 }
