0b41423bdce8b4639a969b2ff06bc5691baa76ea
Dot8Pixels <napatsriz@gmail.com>
2024-02-04 23:05:34 +0700

    feat: :sparkles: References and Borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x: i32 = 1;
+
+    println!("{}", x);
+
+    double(&mut x);
+
+    println!("{}", x);
+}
+
+fn double(x: &mut i32) {
+    *x += *x;
 }
