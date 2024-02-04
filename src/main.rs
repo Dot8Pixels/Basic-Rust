681d962b0290ddc67cef1b55eff912c40356ff63
Dot8Pixels <napatsriz@gmail.com>
2024-02-04 22:19:13 +0700

    feat: :sparkles: Data Types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let message: String = String::from("Hello, world");
+    println!("{}", message);
+
+    let message2: &str = "Hello, world";
+    println!("{}", message2);
+
+    /* Compound Type ------------------------------------------------------------ */
+    // Tuples
+    let tup1: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{:?}", tup1);
+
+    let slice1: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{:?}", slice1);
 }
