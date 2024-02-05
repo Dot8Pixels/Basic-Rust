5e5938718cc557b10d283dcf5904e852f8a8e80c
Dot8Pixels <napatsriz@gmail.com>
2024-02-05 21:01:37 +0700

    feat: :sparkles: Vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let v1: Vec<i32> = vec![1, 2, 3, 4, 5];
+    let mut v2: Vec<i32> = vec![];
+
+    for i in v1.iter() {
+        if is_even(i) {
+            v2.push(*i);
+        }
+    }
+
+    println!("v2: {:?}", v2);
+}
+
+fn is_even(num: &i32) -> bool {
+    num % 2 == 0
 }
