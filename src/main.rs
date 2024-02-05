58059d71052c14511cac2b840a66739fb9fa4950
Dot8Pixels <napatsriz@gmail.com>
2024-02-05 20:52:33 +0700

    feat: :sparkles: Slices

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // for x in xs.iter() {
+    //     println!("x is {}", x);
+    // }
+
+    for (i, v) in xs.iter().enumerate() {
+        println!("{} -> {}", i, v);
+    }
 }
