cd325358fe52703a6e69359473f1757fb9924347
Dot8Pixels <napatsriz@gmail.com>
2024-02-05 22:22:07 +0700

    feat: :sparkles: Enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#[derive(Debug)]
+
+enum PlayerClass {
+    Mage(i32),
+    Warrior(i32),
+}
+
+trait PlayerAction {
+    fn get_weapon(&self);
+}
+
+impl PlayerAction for PlayerClass {
+    fn get_weapon(&self) {
+        match self {
+            Self::Mage(damage) => println!("{}, damage: {}", String::from("Staff"), damage),
+            Self::Warrior(damage) => println!("{}, damage: {}", String::from("Sword"), damage),
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let mage_class: PlayerClass = PlayerClass::Mage(200);
+
+    println!("{:?}", mage_class);
+
+    mage_class.get_weapon();
+
+    let warrior_class: PlayerClass = PlayerClass::Warrior(150);
+
+    println!("{:?}", warrior_class);
+
+    warrior_class.get_weapon()
 }
