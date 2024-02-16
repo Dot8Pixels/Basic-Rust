e6ab6d88b9e84d439d71868fc98609f68ed32a5a
Dot8Pixels <napatsriz@gmail.com>
2024-02-16 21:02:58 +0700

    feat: :sparkles: Lifetime

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+fn max<'a>(x: &'a i32, y: &'a i32) -> &'a i32 {
+    if x > y {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x: i32 = 5;
+    let y: i32 = 10;
+
+    let max_number: &i32 = max(&x, &y);
+
+    println!("{}", max_number)
+
+    // let r: &i32;
+
+    // {
+    //     let x: i32 = 5;
+    //     r = &x;
+
+    //     println!("{}", r);
+    // }
 }
