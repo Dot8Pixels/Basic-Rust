2ca7b0fa4772f7fc1495ceaa9e0f032891750314
Dot8Pixels <napatsriz@gmail.com>
2024-02-18 21:49:19 +0700

    feat: :sparkles: Smart Pointer - Box

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[derive(Debug)]
+
+enum List {
+    Node(i32, Box<List>),
+    Nil,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // let x: i32 = 10;
+    // let y: Box<i32> = Box::new(x);
+    // println!("{:?}", y);
+
+    let list: List = List::Node(1, Box::new(List::Node(2, Box::new(List::Nil))));
+    println!("{:?}", list);
 }
