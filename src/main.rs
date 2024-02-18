b70abe6a997b02234f2779eee201f61ae9b65bd4
Dot8Pixels <napatsriz@gmail.com>
2024-02-18 22:16:56 +0700

    feat: :sparkles: Smart Pointer - RefCell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{cell::RefCell, rc::Rc};
 fn main() {
-    println!("Hello, world!");
+    let x = Rc::new(RefCell::new(Box::new(10)));
+
+    let y = Rc::clone(&x);
+    let z = Rc::clone(&x);
+
+    println!("y: {:?}", y);
+    println!("z: {:?}", z);
+
+    *z.borrow_mut() = Box::new(20);
+
+    println!("x: {:?}", x);
+    println!("y: {:?}", y);
+    println!("z: {:?}", z);
 }
