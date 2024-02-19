bfea49ae91f8e1cdca18267924af525dbded622d
Dot8Pixels <napatsriz@gmail.com>
2024-02-19 20:52:08 +0700

    feat: :sparkles: Match Expressions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::result::Result;
+
+fn get_dog_price(breed: &String) -> Result<u32, String> {
+    match breed.as_str() {
+        "Chihuahua" => Ok(10000),
+        "Labrador" => Ok(20000),
+        "GermanShepherd" => Ok(30000),
+        _ => Err(String::from("Breed not found")),
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let dog: String = String::from("Labrador");
+
+    let price: u32 = match get_dog_price(&dog) {
+        Ok(value) => value,
+        Err(err) => {
+            println!("{}", err);
+            0
+        }
+    };
+
+    println!("{}", price);
 }
