a22ce6f6c037a6b90c7d7957d5214763f917d897
Dot8Pixels <napatsriz@gmail.com>
2024-02-19 21:24:38 +0700

    feat: :sparkles: Concurrency - Thread

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let handle1 = thread::spawn(|| {
+        for i in 0..10 {
+            println!("Hello, thread1: {}", i);
+            thread::sleep(Duration::from_millis(100));
+        }
+    });
+
+    let handle2 = thread::spawn(|| {
+        for i in 0..15 {
+            println!("Hello, thread2: {}", i);
+            thread::sleep(Duration::from_millis(100));
+        }
+    });
+
+    handle1.join().unwrap();
+    handle2.join().unwrap();
+
+    println!("Finished !!!");
 }
